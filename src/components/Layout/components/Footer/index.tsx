c9e7d990a24214bf5713b28f5527c7b99719863c
Adrián <adrian@manjaro.fibertel.com.ar>
2021-04-21 22:10:46 -0300

    Cambios de Adrián

--- src/components/Layout/components/Footer/index.tsx
+++ src/components/Layout/components/Footer/index.tsx
@@ -6,8 +6,8 @@ const Footer: FC = () => {
     let today = new Date();
     let year = today.getFullYear();
     return (
-        <footer className='footer footer-alt text-center mt-auto text-white p-2' style={{ backgroundColor: '#001529', fontSize: 15 }}>
-            <p>Made with < HeartFill className='text-danger' /> Movie App 🎥 - {year} - </p>
+        <footer className='footer footer-alt text-center mt-auto text-white py-3' style={{ backgroundColor: '#001529', fontSize: 15 }}>
+            <p className="m-0">Made with < HeartFill className='text-danger' /> Movie App 🎥 - {year} - </p>
         </footer>
     )
 }
--- src/screens/Details/index.tsx
+++ src/screens/Details/index.tsx
@@ -17,9 +17,9 @@ const Details: FC = () => {
     const posterAvailable = (movie.id !== 0 && movie.poster_path !== null)
     const backdropAvailable = (movie.id !== 0 && movie.backdrop_path !== null)
     const posterImg = <img className='movie-poster' alt='poster' src={`https://image.tmdb.org/t/p/w400${movie.poster_path}`}></img>
-    const backdropImg = <div className='movie-background' style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w1280${movie.backdrop_path}` }}></div>
+    // const backdropImg = <div className='movie-background' style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w1280${movie.backdrop_path}` }}></div>
     const posterImgPlaceholder = <img className='movie-poster' alt='poster' src={imagenotfound}></img>
-    const backdropImgPlaceholder = <div className='movie-background' style={{ backgroundColor: `#3c3c3c` }}></div>
+    // const backdropImgPlaceholder = <div className='movie-background' style={{ backgroundColor: `#3c3c3c` }}></div>
 
     useEffect(() => {
         console.log('id: ' + id)
@@ -34,52 +34,54 @@ const Details: FC = () => {
     // const resultsJsx = () => {
     return (
         <Layout>
-            <div className='details-container'>
+            <div className='details-container' style={{ backgroundImage: backdropAvailable ? `url(https://image.tmdb.org/t/p/w1280${movie.backdrop_path}` : '', backgroundSize: 'cover', backgroundPosition: 'center center' }}>
                 {/* <div className='movie-background' style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w1280${movie.backdrop_path}` }}>
                 </div> */}
-                {backdropAvailable ? backdropImg : backdropImgPlaceholder}
-                <div className='row'>
-                    <div className='col-sm-6 movie-info-poster'>
+                {/* {!backdropAvailable && backdropImgPlaceholder} */}
+                <div className="container">
+                    <div className='row'>
+                        <div className='col-sm-6 movie-info-poster'>
 
-                        {/* <img className='movie-poster' src={`https://image.tmdb.org/t/p/w400${movie.poster_path}`}
-                            alt='movie poster'>
-                        </img> */}
-                        {posterAvailable ? posterImg : posterImgPlaceholder}
-                    </div>
-                    <div className='col-sm-6 movie-info-header'>
-                        <div className='row'>
-                            <h3>{movie.title}</h3>
-                            <span style={{ fontSize: 20 }}>Release Date: {movie.release_date}</span>
-                            <span style={{ fontSize: 20 }}>Rated: {movie.vote_average}</span>
+                            {/* <img className='movie-poster' src={`https://image.tmdb.org/t/p/w400${movie.poster_path}`}
+                                alt='movie poster'>
+                            </img> */}
+                            {posterAvailable ? posterImg : posterImgPlaceholder}
                         </div>
-                        <div className='row'>
-                            <div className='star'>
-                                <StarRatingComponent
-                                    name={`rate` + movie.id}
-                                    starCount={10}
-                                    value={movie.vote_average}
-                                    starColor={'#0066ff'}
-                                ></StarRatingComponent>
+                        <div className='col-sm-6 movie-info-header'>
+                            <div>
+                                <h3>{movie.title}</h3>
+                                <span style={{ fontSize: 20 }}>Release Date: {movie.release_date}</span>
+                                <span style={{ fontSize: 20 }}>Rated: {movie.vote_average}</span>
+                            </div>
+                            <div>
+                                <div className='star'>
+                                    <StarRatingComponent
+                                        name={`rate` + movie.id}
+                                        starCount={10}
+                                        value={movie.vote_average}
+                                        starColor={'#0066ff'}
+                                    ></StarRatingComponent>
+                                </div>
+                            </div>
+                            <div className='movie-info-content'>
+                                <h5>Overview</h5>
+                                <p className='overview'>{movie.overview}</p>
+                                {movie.genres &&
+                                    <>
+                                        <h5>Genres</h5>
+                                        <ul>
+                                            {movie.genres.map(genre =>
+                                                <li className='gerne-list' key={genre.id}>{genre.name}</li>)}
+                                        </ul>
+                                    </>
+                                }
+                                <div className='movie-trailer'>
+                                    <MovieTrailer />
+                                </div>
                             </div>
                         </div>
-                        <div className='row movie-info-content'>
-                            <h5>Overview</h5>
-                            <p className='overview'>{movie.overview}</p>
-                            {movie.genres &&
-                                <>
-                                    <h5>Genres</h5>
-                                    <ul>
-                                        {movie.genres.map(genre =>
-                                            <li className='gerne-list' key={genre.id}>{genre.name}</li>)}
-                                    </ul>
-                                </>
-                            }
-                            <div className='movie-trailer'>
-                                <MovieTrailer />
-                            </div>
-                        </div>
-                    </div>
 
+                    </div>
                 </div>
             </div>
         </Layout>
